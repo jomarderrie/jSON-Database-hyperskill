ff371b222173297ee39c946929d6c7bc5d46baa8
Odysseus <64851813+NathanGanesh@users.noreply.github.com>
2021-05-21 15:38:53 +0200

    test

--- server/Main.java
+++ server/Main.java
@@ -1,6 +1,7 @@
 package server;
 
 import com.google.gson.*;
+import server.database.DatabaseConnection;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
@@ -14,28 +15,44 @@ import java.util.concurrent.Executors;
 
 public class Main {
     private static final int PORT = 34523;
-
-    private static final ExecutorService executor = Executors.newCachedThreadPool();
+    private ServerSocket serverSocket;
+    private  final ExecutorService executor = Executors.newCachedThreadPool();
 
     public static void main(String[] args) throws IOException {
+        new Main().run();
+    }
+
+    public void run() throws IOException {
         System.out.println("Server started!");
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
-        ServerSocket serverSocket = new ServerSocket(PORT);
+        serverSocket = new ServerSocket(PORT);
+        DatabaseConnection connection = new DatabaseConnection("C:\\Users\\kimjongun\\IdeaProjects\\JSON Database\\JSON Database\\task\\src\\server\\data\\db.json", gson);
         while (!serverSocket.isClosed()){
             Socket socket = serverSocket.accept();
             executor.submit(() -> {
                 try (DataInputStream in = new DataInputStream(socket.getInputStream());
                      DataOutputStream out = new DataOutputStream(socket.getOutputStream())) {
                     String command = in.readUTF();
-//                    System.out.println(Thread.currentThread().getName() + " : received: " + command);
                     Task task = new Gson().fromJson(command, Task.class);
-                    System.out.println(command);
-//                    OperationFactory factory = new OperationFactory(task, this, connection);
+                    OperationFactory factory = new OperationFactory(task, this, connection);
+                    ServerResp serverResp = factory.createOperation().execute();
 
+                    String textResponse = new GsonBuilder().create().toJson(serverResp);
+                    out.writeUTF(textResponse);
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
             });
         }
+    }
+
+    public ServerResp close() {
+        this.executor.shutdown();
+        try {
+            serverSocket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return ServerResp.OK;
     }
 }
